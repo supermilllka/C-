aaeb96b8702bfe7aa05b8e5be70f70810f18273d
supermilllka <milka.87@bk.ru>
2022-12-17 15:36:12 +0400

    Добавили изменения

--- example010/Program.cs
+++ example010/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//имеется одномерный массив array из n
+// элементов, требуется найти элемент 
+//массива, равный find
+
+int[] array = { 1, 12, 31, 4, 18, 16, 17, 18 };
+
+int n = array.Length;
+ int find = 18;
+
+ int index = 0;
+ while (index<n)
+ {
+if (array[index]==find)
+{
+    Console.WriteLine(index);
+    break;
+}
+
+    index++;
+ }
